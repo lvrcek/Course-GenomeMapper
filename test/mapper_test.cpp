ddc3f4cb63012eabd8ed2e780320f730f9bbd3d7
Lovro Vrcek <vrcek.lovro@gmail.com>
2021-07-21 04:52:45 +0800

    Added googletest

---
+++ test/mapper_test.cpp
@@ -0,0 +1,13 @@
+#include "bioparser/fasta_parser.hpp"
+#include "bioparser/fastq_parser.hpp"
+#include <gtest/gtest.h>
+
+// Test googletest integration
+TEST(BasicTest, HandlesZeroInput) {
+    ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
